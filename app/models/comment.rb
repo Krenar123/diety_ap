0a939152c9862f7b6bcb5c887082280d6755d4bc
Krenar123 <krenardardhishta205@gmail.com>
2020-08-21 00:37:35 +0200

    Added validations to Comment

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,8 @@
 class Comment < ApplicationRecord
+  MAX_BODY_LENGTH = 500
+
   belongs_to :article
   belongs_to :user
+
+  validates :body, presence: true, length: { maximum: MAX_BODY_LENGTH }
 end
