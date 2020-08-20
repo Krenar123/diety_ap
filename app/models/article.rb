0de25e5778c4cf3960421e89baa6947ce593565d
Krenar123 <krenardardhishta205@gmail.com>
2020-08-21 00:35:52 +0200

    Added validations for articles

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,10 @@
 class Article < ApplicationRecord
+    MAX_TITLE_LENGTH = 120
+    MAX_BODY_LENGTH = 2200
+
     belongs_to :user
     has_many :comments, dependent: :destroy
+
+    validates :title, presence: true, length: { maximum: MAX_TITLE_LENGTH }
+    validates :body, length: { maximum: MAX_BODY_LENGTH }
 end
